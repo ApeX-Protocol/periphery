6a3bd40b022dfe63bebdb3008e7a4296fb7ffb1a
keeganlee <keegan.lee704@gmail.com>
2022-06-03 15:14:50 +0800

    deploy merkle

--- scripts/deploy_merkle.js
+++ scripts/deploy_merkle.js
@@ -1,14 +1,12 @@
-const { ethers, upgrades } = require("hardhat");
-const { BigNumber } = require("@ethersproject/bignumber");
 const verifyStr = "npx hardhat verify --network";
 
-let apeXToken = "0x61A1ff55C5216b636a294A07D77C6F4Df10d3B56";
-let merkleRoot = "0xcf71d38f74a6c30da225d9bf93250f5e9af9d09cb17a6b562dc6a4d49c353980";
+// let apeXToken = "0x61A1ff55C5216b636a294A07D77C6F4Df10d3B56"; // ArbitrumOne
+let apeXToken = "0x3f355c9803285248084879521AE81FF4D3185cDD"; // Arbitrum Testnet
+let merkleRoot = "0x0bcf838a06ebcf3f2fcb9065bf84b8fa2daefe4be5a5e0275e09cd62c624c070";
 let merkleDistributor;
 
 const main = async () => {
-  // await createInvitation();
-  await createRewardForCashback();
+  await createMerkleDistributor();
 };
 
 async function createMerkleDistributor() {
